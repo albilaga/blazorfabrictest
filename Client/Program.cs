a5d8ef7229f2d67c10706fd5c2e6295164517832
Albilaga <albilaga.lp@gmail.com>
2020-05-14 13:53:53 +0700

    Initial Commit.

---
+++ Client/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BlazorFabric;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace blazorfabrictest.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddBlazorFabric();
+            builder.RootComponents.Add<GlobalRules>("#staticcs");
+            //builder.Services.AddHttpClient("blazorfabrictest.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            //    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            //builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("blazorfabrictest.ServerAPI"));
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                //options.ProviderOptions.DefaultAccessTokenScopes.Add("api://api.id.uri/user_impersonation");
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ tests/blazorfabrictest.tests/MainLayoutTest.cs
@@ -0,0 +1,26 @@
+﻿using BlazorFabric;
+using blazorfabrictest.Client.Shared;
+using Bunit;
+using Bunit.Mocking.JSInterop;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace blazorfabrictest.tests
+{
+    public class MainLayoutTest : ComponentTestFixture
+    {
+        [Fact]
+        public void Test1()
+        {
+            Services.AddBlazorFabric();
+            Services.AddMockJsRuntime();
+            Services.AddOptions()
+                .AddAuthorizationCore()
+                .AddLogging()
+                .AddSingleton<SignOutSessionStateManager>();
+                //.AddSingleton<AuthenticationStateProvider>();
+            var mainLayout = RenderComponent<MainLayout>();
+        }
+    }
+}
